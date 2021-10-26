047cd383066f9b5ad727c8845855138dcbc18116
ApolloUnicorn <nehomesley@gmail.com>
2021-10-26 18:35:46 -0400

    add fee to transaction

--- contracts/token/ERC1155/presets/EyeLid.sol
+++ contracts/token/ERC1155/presets/EyeLid.sol
@@ -20,16 +20,25 @@ contract EyeLid is EyeCon, Ownable {
     uint256[] private qty = [10000000, 1]; 
     uint256 private _world_pop = 0;
     uint256[] private ids = [0,NFT_id];
-
-    constructor(string memory uri)
+    uint public price = 10000000 wei;
+    address payable public artist;
+    constructor(string memory uri, address payable artist_)
         EyeCon(uri)
-    {}
+    {
+        artist = artist_;
+    }
 
-    function mint_eyecon() public virtual returns (uint256) {
+    function mint_eyecon() public payable virtual returns (uint256) {
+
+        //Limit total number of NFTs to the current world_pop(in Billions of people) 
         require(NFT_id < world_pop(), "EyeLid: world population set too low to mint more");
+
+        //Require mint_eyecon to include the current price
+        require(msg.value >= price, "EyeLid: please include the current price with your transaction");
             NFT_id ++;
             address buyer = _msgSender();
             mintBatch(buyer, ids, qty, "0x00");
+            artist.transfer(price);
             return NFT_id;
     }
 
@@ -42,4 +51,15 @@ contract EyeLid is EyeCon, Ownable {
         require (new_pop > _world_pop, "EyeLid: population can only be increased");
         _world_pop = new_pop;
     }
+
+    function set_price(uint256 new_price) public virtual {
+        require (hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "EyeLid: must have admin role to change price");
+        price = new_price;
+    }
+
+    function set_artist(address payable _artist) public virtual {
+        require (hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "EyeLid: must have admin role to change artist");
+        artist = _artist;
+    }
+    
 }
