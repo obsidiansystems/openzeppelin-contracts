6d236e038c09bdb9459edb18d40740027c22c055
ApolloUnicorn <nehomesley@gmail.com>
2021-10-28 14:45:20 -0400

    rename some functions

--- contracts/token/ERC1155/presets/EyeLid.sol
+++ contracts/token/ERC1155/presets/EyeLid.sol
@@ -32,47 +32,50 @@ contract EyeLid is EyeCon, Ownable {
         _artist = payable(_msgSender());
     }
 
-    function mint_eyecon() public payable virtual returns (uint256) {
+    function mintEyecon() public payable virtual returns (uint256) {
 
         //Limit total number of NFTs to the current world_pop(in Billions of people) 
-        require(NFT_id < world_pop(), "EyeLid: world population set too low to mint more");
+        require(NFT_id < worldPop(), "EyeLid: world population set too low to mint more");
 
         //Require mint_eyecon to include the current price
-        require(msg.value >= _price, "EyeLid: please include the current price with your request");
+//        require(msg.value >= _price, "EyeLid: please include the current price with your request");
             NFT_id ++;
             address buyer = _msgSender();
             ids = [0, NFT_id];
-            mintBatch(buyer, ids, qty, "");
+            super.mintBatch(buyer, ids, qty, "");
             _artist.transfer(msg.value);
             return NFT_id;
     }
 
-    function world_pop() public view returns (uint256) {
+    function worldPop() public view returns (uint256) {
         return _world_pop;
     }
 
-    function set_world_pop(uint256 new_pop) public virtual {
+    function setWorldPop(uint256 new_pop) public virtual returns (bool) {
         require (hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "EyeLid: must have admin role to change population");
         require (new_pop > _world_pop, "EyeLid: population can only be increased");
         _world_pop = new_pop;
+        return true;
     }
 
     function price() public view returns (uint256) {
         return _price;
     }
 
-    function set_price(uint256 new_price) public virtual {
+    function setPrice(uint256 new_price) public virtual returns (bool) {
         require (hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "EyeLid: must have admin role to change price");
         _price = new_price;
+        return true;
     }
 
     function artist() public view returns (address payable) {
         return _artist;
     }
 
-    function set_artist(address payable new_artist) public virtual {
+    function setArtist(address payable new_artist) public virtual returns (bool) {
         require (hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "EyeLid: must have admin role to change artist");
         _artist = new_artist;
+        return true;
     }
     
 }
